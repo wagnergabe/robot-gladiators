1b3edae58ec8442b5cfca411905a2d7e7d01a325
GSvenny <gabrielswagner7@gmail.com>
2022-05-11 23:14:03 -0500

    added shop function

--- assets/js/game.js
+++ assets/js/game.js
@@ -8,7 +8,7 @@ var playerMoney = 10;
 //---NPC STATS---///
 var enemyNames = ['Roborto', 'Amy Android', 'Robo Trumble'];
 var enemyHealth = 50;
-var enemyAttack = 40;
+var enemyAttack = 12;
 
 
 //---This creates a function named "fight"---//
@@ -66,6 +66,13 @@ for (var i = 0; i < enemyNames.length; i++) {
     var pickedEnemyName = enemyNames[i];
     enemyHealth = 50;
     fight(pickedEnemyName);
+
+    if(playerHealth > 0 && i < enemyNames.length - 1) {
+        var storeConfirm = window.confirm("The fight is over, visit the shop before the next round?");
+        if (storeConfirm) {
+        shop();
+        }
+    }
   }
   else {
       window.alert("Your robot has been defeated :(");
@@ -74,7 +81,7 @@ for (var i = 0; i < enemyNames.length; i++) {
   }
   //play again
   endGame();
-}
+};
 
 var endGame = function() {
     if (playerHealth > 0) {
@@ -91,7 +98,47 @@ var endGame = function() {
         else {
             window.alert("Thank you for playing! Come back again soon!");
         }
-}
+};
+//--Shop--//
+
+var shop = function () {
+    var shopOptionPrompt = window.prompt(
+        "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? please enter one: 'REFILL', 'UPGRADE', or 'LEAVE' to make a choice"
+        );
+
+    switch (shopOptionPrompt) {
+        case "refill":
+        case "REFILL":
+            if (playerMoney >=7) {
+            window.alert("Refilling player's health by 20 for 7 coins.");
+            playerHealth = playerHealth + 20;
+            playerMoney = playerMoney - 7;
+            }
+            else {
+                window.alert ("Sorry, you dont have the coins!");
+            }
+            break;
+        case "upgrade":
+        case "UPGRADE":
+            if (playerMoney >=7) {
+            window.alert("Upgrading player's attack by 6 for 7 coins");
+            playerAttack = playerAttack + 6;
+            playerMoney = playerMoney - 7;
+            }
+            else {
+                window.alert("Come back when you have more coins!");
+            }
+            break;
+        case "leave":
+        case "LEAVE":
+            window.alert("Leaving the shop");
+            break;
+        default:
+            window.alert("You did not pick a valid option. Try again.");
+            shop();
+            break;
+    }
+};
 
 //Start Game//
 startGame();
